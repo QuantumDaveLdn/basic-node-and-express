29120f06ba111def58964e82d0aa99882a026d78
QuantumDaveLdn <davronbecki44@gmail.com>
2025-05-15 19:07:36 +0100

    Add root-level logger middleware
    - Implemented root-level middleware to log request method, path, and IP
    - Added middleware that follows the format: METHOD PATH - IP
    - Positioned middleware to properly log all incoming requests
    - Ensured middleware properly calls next() to continue the request chain

--- myApp.js
+++ myApp.js
@@ -1,39 +1,28 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
+console.log("Hello World");
+app.get('/', function (req, res) {
+    const absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get("/json", function(req, res) {
+    let message = "Hello json";
 
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
 
+    res.json({"message": message});
+});
 
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+}); 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
