b47a9210d8f519f3e4c60f416a18ea3a6339295e
QuantumDaveLdn <davronbecki44@gmail.com>
2025-05-16 14:02:04 +0100

    Complete all Basic Node and Express challenges
    - Implemented chain middleware for time server
    - Added route parameter handling with /:word/echo endpoint
    - Added form processing with body-parser
    - Created POST endpoint for name submission
    - Completed all 12 challenges in this section

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 let express = require('express');
+let bodyParser =  require("body-parser");
 let app = express();
 console.log("Hello World");
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
@@ -24,6 +27,28 @@ app.get("/json", function(req, res) {
     }
 
     res.json({"message": message});
+});
+
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString();
+    next(); 
+}, function (req, res){
+    res.json({"time": req.time});
+});
+
+app.get("/:word/echo", function(req, res) {
+    const word = req.params.word;
+    res.json({"echo": word});
+});
+
+app.post("/name", function(req, res) {
+    if (req.body.first && req.body.last) {
+        const response = {"name": `${req.body.first} ${req.body.last}`}
+        res.json(response);
+    } else {
+        res.json({"error": "Cant find name or last name"});
+    }
+
 });
 
 module.exports = app;
